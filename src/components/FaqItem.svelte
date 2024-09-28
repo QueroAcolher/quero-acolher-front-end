d4adf2189faa3cce1c11fc4fcadd4d946235a8d1
karl Thomas <karlthomas3@icloud.com>
2024-09-28 15:39:01 +0100

    added FaqItem and FAQ page now displays items from faq_en.json

---
+++ src/components/FaqItem.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let question;
+  export let answer;
+</script>
+
+<div class="faq-item mb-6 border-b border-gray-300 pb-4">
+  <h2 class="mb-2 text-lg font-bold">{question}</h2>
+  <p class="text-gray-700">{@html answer}</p>
+</div>
